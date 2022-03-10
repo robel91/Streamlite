97160cfe3d4fd94142dbaa6520441ec8536beca3
robel91 <92173230+robel91@users.noreply.github.com>
2022-03-09 23:35:34 -0500

    Update price.py

--- price.py
+++ price.py
@@ -63,7 +63,7 @@ if selection == 'Data Visualization & Model Prediction':
     def show_pdf(file):
         with open(file, 'rb') as f:
             base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        pdf_display = f'<iframe src ="data:application/pdf;base64,{base64_pdf}" width = "800" height = "800" type="application/pdf"></iframe>'
+        pdf_display = f'<iframe src ="data:application/pdf;base64,{base64_pdf}" width = "700" height = "1000" type="application/pdf"></iframe>'
 
         #Display file
         st.markdown(pdf_display,unsafe_allow_html=True)
