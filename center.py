c29b5bd9ba3c09bf02d635aeff56d70b5170a317
robel91 <92173230+robel91@users.noreply.github.com>
2022-03-10 20:40:51 -0500

    Update center.py

--- center.py
+++ center.py
@@ -2,8 +2,7 @@ import pandas as pd
 import streamlit as st
 import base64
 
-st.header ('''Prediction Model Powered By Machine Learning 
-               By Robel Mebrahtu:muscle::muscle:''')
+st.header ('''Prediction Model Powered By Machine Learning By Robel Mebrahtu:muscle::muscle:''')
 
 #Sidebar
 selection = st.sidebar.radio("HOME",
